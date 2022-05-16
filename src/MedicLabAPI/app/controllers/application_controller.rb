f7ea822096c416e1b81c13bfbe9e86e4c8f24f6a
png331 <g.al-batnidzhi@hotmail.com>
2022-05-16 12:19:09 +0300

    ENV var changes to database.yml

--- src/MedicLabAPI/app/controllers/application_controller.rb
+++ src/MedicLabAPI/app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 class ApplicationController < ActionController::API
-    # before_action :configure_permitted_parameters, if: :devise_controller?
-
-
-
-    # protected
     
-    # def configure_permitted_parameters
-    #     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :dob, :address, :password, :password_confirmation, :role_id])
-    # end
     def is_admin?
         current_user.role.name == "Admin"
     end
